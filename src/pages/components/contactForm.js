fa709dfde45823df4442688e9cb5183fcbed84c6
Jose <cote99salamanca@gmail.com>
2020-06-05 00:09:50 -0400

    prueba

--- src/pages/components/contactForm.js
+++ src/pages/components/contactForm.js
@@ -2,16 +2,16 @@ import React from'react';
 //{Fragment, useState}
 export default () =>{
     return (
-        <form className="mt-16 text-center" method="POST" id="contactForm" name="contactForm" data-netlify="true" netlify-honeypot="bot-field">
+        <form className="mt-16 text-center" method="post" id="contactForm" name="contactForm" data-netlify="true" netlify-honeypot="bot-field">
             <label className="block text-lg font-bold text-blue-700 mb-2" htmlFor="contact-content">Cuentame de esa idea que quieres hacer realidad: 
             </label>
-            <p id="status"></p>
             <div className="flex shadow rounded bg-white border p-2">
-                <textarea 
+                {/* <textarea 
                 placeholder="Cuentame sobre ti y tu idea..."
                 id="contact-content"
                 name="contact-content"
-                className="flex-1 py-2 px-3 focus:outline-none focus:shadow-outline"></textarea>
+                className="flex-1 py-2 px-3 focus:outline-none focus:shadow-outline"></textarea> */}
+                <input type="text" name="name" placeholder="cuentame sobre  ti..." />
                 <button className="btn ml-4" id="formBtn">Enviar</button>
             </div>
         </form>
